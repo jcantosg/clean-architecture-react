e3c5fa3209184ab38103aa82d8ed0f2dc5c8ec31
jcantosg <josel.cantos@universae.com>
2024-05-17 00:19:44 +0200

    Configure githook

--- src/pages/__tests__/ProductPage.spec.tsx
+++ src/pages/__tests__/ProductPage.spec.tsx
@@ -3,6 +3,6 @@ import { render, screen } from "@testing-library/react";
 import App from "../../App";
 
 test("Loads and displays title", async () => {
-  render(<App />);
-  await screen.findAllByRole("heading", { name: "Product price updater" });
+    render(<App />);
+    await screen.findAllByRole("heading", { name: "Product price updater" });
 });
