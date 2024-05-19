5975ab7f0a8015807983b71b023645d4826806f8
jcantosg <josel.cantos@universae.com>
2024-05-19 12:49:37 +0200

    Create custom render

--- src/pages/__tests__/ProductPage.spec.tsx
+++ src/pages/__tests__/ProductPage.spec.tsx
@@ -1,8 +1,14 @@
 import { test } from "vitest";
-import { render, screen } from "@testing-library/react";
-import App from "../../App";
+import {render, RenderResult, screen} from "@testing-library/react";
+import {ProductsPage} from "../ProductsPage.tsx";
+import {ReactNode} from "react";
+import {AppProvider} from "../../context/AppProvider.tsx";
 
 test("Loads and displays title", async () => {
-    render(<App />);
+    renderComponent(<ProductsPage />);
     await screen.findAllByRole("heading", { name: "Product price updater" });
 });
+
+function renderComponent(component: ReactNode): RenderResult {
+    return render(<AppProvider>{component}</AppProvider>)
+}
